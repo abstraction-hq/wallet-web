e7edd7fd76f4c593c4f992a0b5c54779d36d24b8
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-20 21:11:59 +0700

    test passkey

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,72 @@
+"use client"
 import Image from "next/image";
 
 export default function Home() {
+
+  const createPassKey = async () => {
+    const message = "hello world"
+    const encoder = new TextEncoder();
+    const messageBuffer = encoder.encode(message);
+
+    // Generate the challenge based on the message
+    const challenge = new Uint8Array(messageBuffer);
+
+    // Get the credential for signing
+    const assertion = await navigator.credentials.create({
+      publicKey: {
+        user: {
+          id: new Uint8Array([1, 2, 3, 4, 5]),
+          name: "imduchuyyy",
+          displayName: "imduchhuyyy"
+        },
+        challenge: challenge,
+        pubKeyCredParams: [{ type: 'public-key', alg: -7 }],
+        rp: {
+          name: "Example Corp"
+        }
+      }
+    });
+
+    console.log(assertion)
+  }
+
+  const signWithPasskey = async () => {
+    const message = "hello world"
+    const encoder = new TextEncoder();
+    const messageBuffer = encoder.encode(message);
+
+    // Generate the challenge based on the message
+    const challenge = new Uint8Array(messageBuffer);
+
+    // Get the credential for signing
+    const assertion = await navigator.credentials.get({
+      publicKey: {
+        challenge: challenge,
+        timeout: 60000,
+        userVerification: "preferred",
+        allowCredentials: [
+        ],
+      },
+    });
+
+    console.log(assertion)
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <button
+          className="flex items-center gap-2 p-4 rounded-lg border border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
+          onClick={createPassKey}
+        >
+          Create Passkey
+        </button>
+        <button
+          className="flex items-center gap-2 p-4 rounded-lg border border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
+          onClick={signWithPasskey}
+        >
+          Sign with Passkey
+        </button>
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
