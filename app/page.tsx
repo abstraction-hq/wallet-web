c724ad740a3fe6b33cc3f9c578acb6a07f903e81
imduchuyyy <duchuy.124dk@gmail.com>
2024-06-20 23:14:25 +0700

    webauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,47 @@
-"use client"
-import Image from "next/image";
+"use client";
+import { client, utils, parsers } from "@passwordless-id/webauthn";
+import { AuthenticationEncoded, RegistrationEncoded, RegisterOptions, AuthenticateOptions } from '@passwordless-id/webauthn/dist/esm/types'
 
 export default function Home() {
+  const register = (
+    challenge: string,
+    name?: string,
+    options?: RegisterOptions
+  ): Promise<RegistrationEncoded> => {
+    return client.register(
+      name ? name : utils.randomChallenge(),
+      challenge,
+      options
+    );
+  };
 
   const createPassKey = async () => {
-    const message = "hello world"
-    const encoder = new TextEncoder();
-    const messageBuffer = encoder.encode(message);
+    const payload = utils.randomChallenge();
+    const name = "imduchuyyy";
 
-    // Generate the challenge based on the message
-    const challenge = new Uint8Array(messageBuffer);
-
-    // Get the credential for signing
-    const assertion = await navigator.credentials.create({
-      publicKey: {
-        user: {
-          id: new Uint8Array([1, 2, 3, 4, 5]),
-          name: "imduchuyyy",
-          displayName: "imduchhuyyy"
-        },
-        challenge: challenge,
-        pubKeyCredParams: [{ type: 'public-key', alg: -7 }],
-        rp: {
-          name: "Example Corp"
-        }
-      }
+    const regData = await register(payload, name, {
+      authenticatorType: "both",
+      userVerification: "required",
     });
+    const parsedData = parsers.parseRegistration(regData);
 
-    console.log(assertion)
-  }
+    let pkey = await WebAuthnUtils.getPublicKeyFromBytes(
+      parsedData.credential.publicKey
+    );
+    return new PassKeyKeyPair(
+      parsedData.credential.id,
+      pkey[0],
+      pkey[1],
+      this,
+      name,
+      parsedData.authenticator.aaguid,
+      parsedData.authenticator.name,
+      Date.now()
+    );
+  };
 
   const signWithPasskey = async () => {
-    const message = "hello world"
+    const message = "hello world";
     const encoder = new TextEncoder();
     const messageBuffer = encoder.encode(message);
 
@@ -44,12 +54,11 @@ export default function Home() {
         challenge: challenge,
         timeout: 60000,
         userVerification: "preferred",
-        allowCredentials: [
-        ],
+        allowCredentials: [],
       },
     });
 
-    console.log(assertion)
+    console.log(assertion);
   };
 
   return (
@@ -67,87 +76,6 @@ export default function Home() {
         >
           Sign with Passkey
         </button>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
