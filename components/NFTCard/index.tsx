fa5e15acaa1ef804962d5794cc71cfa8705bb91b
Mien PV <nevermind271999@gmail.com>
2024-07-05 22:02:08 +0700

    fix UI nft

--- components/NFTCard/index.tsx
+++ components/NFTCard/index.tsx
@@ -5,12 +5,13 @@ import Image from "@/components/Image";
 
 interface NFTCardProps {
   nft: NFT;
+  isDarkMode?: boolean;
 }
 
 const MAX_LENGTH = 8;
 
-const NFTCard: React.FC<NFTCardProps> = ({ nft }) => (
-  <div className="shadow-lg rounded-lg overflow-hidden">
+const NFTCard: React.FC<NFTCardProps> = ({ nft, isDarkMode }) => (
+  <div style={{backgroundColor: isDarkMode? '#202020' : '#FFFFFF'}} className="shadow-lg rounded-lg overflow-hidden">
     <div className="relative">
       <Image
         className="object-cover"
@@ -21,14 +22,14 @@ const NFTCard: React.FC<NFTCardProps> = ({ nft }) => (
       />
     </div>
     <div className="border-t px-4 py-2 flex justify-between items-center">
-      <div className="ml-2 text-body-1m text-theme-tertiary font-bold">
+      <div className="text-body-1m text-theme-primary font-bold">
         {nft.name}
       </div>
     </div>
     <div className="border-t px-4 py-2 flex justify-between items-center">
       <div>
-        <span className="text-black">Token ID</span>
-        <div className="text-black font-bold">
+        <span className="text-theme-tertiary font-medium">Token ID</span>
+        <div className="text-theme-primary font-bold">
           {nft.id.toString().length > MAX_LENGTH
             ? `${nft.id.toString().substring(0, MAX_LENGTH)}...`
             : nft.id}
