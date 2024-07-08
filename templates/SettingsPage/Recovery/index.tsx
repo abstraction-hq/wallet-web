7651e3036de6bebd322c0d8c0ccb101e1ea57086
imduchuyyy <duchuy.124dk@gmail.com>
2024-07-08 18:34:31 +0700

    feat: Add recover email field to Recovery section in SettingsPage

--- templates/SettingsPage/Recovery/index.tsx
+++ templates/SettingsPage/Recovery/index.tsx
@@ -8,15 +8,48 @@ type RecoveryProps = {};
 
 const Recovery = ({}: RecoveryProps) => {
   const [walletMetamask, setWalletMetamask] = useState("0x1e86...4d533B");
+  const [recoverMail, setRecoverMail] = useState<string>("contacts@abstraction.world");
 
   return (
     <Details
       title="Recovery"
-      description="Connect external wallet for recovery your smart wallet if you lost all devices."
+      description="Use one of the methods below as a recovery method in case you lose the wallet."
       image="/images/profile-wallets.png"
       colorImage="bg-theme-brand-100"
     >
       <div className="space-y-6">
+        <div className="">
+          <div className="flex items-center mb-2 text-base-2">
+            <div className="mr-3">
+              <Image
+                className="w-6 opacity-100"
+                src="/images/mail.png"
+                width={24}
+                height={24}
+                alt=""
+              />
+            </div>
+            Recover Mail
+          </div>
+          <div className="relative">
+            <button className="group absolute top-0 left-0 bottom-0 z-1 w-12 pl-1 text-0">
+              <Icon
+                className="!w-5 !h-5 fill-theme-tertiary transition-colors group-hover:fill-theme-primary"
+                name="copy-1"
+              />
+            </button>
+            <Field
+              className="flex-1"
+              classInput="pl-12 pr-42 truncate"
+              readOnly
+              value={recoverMail}
+              onChange={(e) => setRecoverMail(e.target.value)}
+            />
+            <button className="btn absolute right-1 bottom-1 w-38 h-10 bg-theme-on-surface-3 rounded-lg text-theme-primary hover:bg-theme-primary hover:text-theme-light">
+              Change Email
+            </button>
+          </div>
+        </div>
         <div className="">
           <div className="flex items-center mb-2 text-base-2">
             <div className="mr-3">
