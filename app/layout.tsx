1d2681249dead93460d790c8f4c7f3659cc59e42
misterbo271 <nevermind271999@gmail.com>
2024-07-23 12:37:16 +0700

    replace og

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,50 +39,137 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/images/favicon.svg" />
-        {/* Description no longer than 155 characters */}
-        <meta name="description" content="Abstraction Wallet" />
-        {/* Product Name */}
-        <meta name="product-name" content="Abstraction Wallet" />
-        {/* Twitter Card data */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@abstraction-hq" />
-        <meta name="twitter:title" content="Abstraction Wallet" />
-        <meta
+    <head>
+      <link rel="icon" href="/images/favicon.svg" />
+      {/* Description no longer than 155 characters */}
+      <meta
+          name="description"
+          content="Abstraction Wallet"
+      />
+      {/* Product Name */}
+      <meta
+          name="product-name"
+          content="Abstraction Wallet"
+      />
+      {/* Twitter Card data */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@ui8" />
+      <meta
+          name="twitter:title"
+          content="Abstraction Wallet"
+      />
+      <meta
           name="twitter:description"
           content="Your crypto gateway to the future"
-        />
-        <meta name="twitter:creator" content="@abstraction-hq" />
-        {/* Twitter Summary card images must be at least 120x120px */}
-        <meta name="twitter:image" content="/images/logo-dark.svg" />
-        {/* Open Graph data for Facebook */}
-        <meta property="og:title" content="Abstraction Wallet" />
-        <meta property="og:type" content="Article" />
-        <meta property="og:url" content="https://wallet.abstraction.world" />
-        <meta property="og:image" content="/images/logo-dark.svg" />
-        <meta
+      />
+      <meta name="twitter:creator" content="@ui8" />
+      {/* Twitter Summary card images must be at least 120x120px */}
+      <meta
+          name="twitter:image"
+          content="/images/opengraph/og_twitter.png"
+      />
+      {/* Open Graph data for Facebook */}
+      <meta
+          property="og:title"
+          content="Abstraction Wallet"
+      />
+      <meta property="og:type" content="Article" />
+      <meta
+          property="og:url"
+          content="https://abstraction.world"
+      />
+      <meta
+          property="og:image"
+          content="/images/opengraph/og_article.png"
+      />
+      <meta
           property="og:description"
           content="Your crypto gateway to the future"
-        />
-        <meta property="og:site_name" content="Abstraction Wallet" />
-        {/* Open Graph data for LinkedIn */}
-        <meta property="og:title" content="Abstraction Wallet" />
-        <meta property="og:url" content="https://wallet.abstraction.world" />
-        <meta property="og:image" content="/images/logo-dark.svg" />
-        <meta
+      />
+      <meta
+          property="og:site_name"
+          content="Abstraction Wallet"
+      />
+      <meta property="fb:admins" content="132951670226590" />
+      {/* Open Graph data for LinkedIn */}
+      <meta
+          property="og:title"
+          content="Abstraction Wallet"
+      />
+      <meta
+          property="og:url"
+          content="https://abstraction.world"
+      />
+      <meta
+          property="og:image"
+          content="/images/opengraph/og_linkedin.png"
+      />
+      <meta
           property="og:description"
           content="Your crypto gateway to the future"
-        />
-        {/* Open Graph data for Pinterest */}
-        <meta property="og:title" content="Abstraction Wallet" />
-        <meta property="og:url" content="https://wallet.abstraction.world" />
-        <meta property="og:image" content="/images/logo-dark.svg" />
-        <meta
+      />
+      {/* Open Graph data for Pinterest */}
+      <meta
+          property="og:title"
+          content="Abstraction Wallet"
+      />
+      <meta
+          property="og:url"
+          content="https://abstraction.world"
+      />
+      <meta
+          property="og:image"
+          content="/images/opengraph/og_printest.png"
+      />
+      <meta
           property="og:description"
           content="Your crypto gateway to the future"
-        />
-      </head>
+      />
+    </head>
+      {/*<head>*/}
+      {/*  <link rel="icon" href="/images/favicon.svg" />*/}
+      {/*  /!* Description no longer than 155 characters *!/*/}
+      {/*  <meta name="description" content="Abstraction Wallet" />*/}
+      {/*  /!* Product Name *!/*/}
+      {/*  <meta name="product-name" content="Abstraction Wallet" />*/}
+      {/*  /!* Twitter Card data *!/*/}
+      {/*  <meta name="twitter:card" content="summary" />*/}
+      {/*  <meta name="twitter:site" content="@abstraction-hq" />*/}
+      {/*  <meta name="twitter:title" content="Abstraction Wallet" />*/}
+      {/*  <meta*/}
+      {/*    name="twitter:description"*/}
+      {/*    content="Your crypto gateway to the future"*/}
+      {/*  />*/}
+      {/*  <meta name="twitter:creator" content="@abstraction-hq" />*/}
+      {/*  /!* Twitter Summary card images must be at least 120x120px *!/*/}
+      {/*  <meta name="twitter:image" content="/images/logo-dark.svg" />*/}
+      {/*  /!* Open Graph data for Facebook *!/*/}
+      {/*  <meta property="og:title" content="Abstraction Wallet" />*/}
+      {/*  <meta property="og:type" content="Article" />*/}
+      {/*  <meta property="og:url" content="https://wallet.abstraction.world" />*/}
+      {/*  <meta property="og:image" content="/images/logo-dark.svg" />*/}
+      {/*  <meta*/}
+      {/*    property="og:description"*/}
+      {/*    content="Your crypto gateway to the future"*/}
+      {/*  />*/}
+      {/*  <meta property="og:site_name" content="Abstraction Wallet" />*/}
+      {/*  /!* Open Graph data for LinkedIn *!/*/}
+      {/*  <meta property="og:title" content="Abstraction Wallet" />*/}
+      {/*  <meta property="og:url" content="https://wallet.abstraction.world" />*/}
+      {/*  <meta property="og:image" content="/images/logo-dark.svg" />*/}
+      {/*  <meta*/}
+      {/*    property="og:description"*/}
+      {/*    content="Your crypto gateway to the future"*/}
+      {/*  />*/}
+      {/*  /!* Open Graph data for Pinterest *!/*/}
+      {/*  <meta property="og:title" content="Abstraction Wallet" />*/}
+      {/*  <meta property="og:url" content="https://wallet.abstraction.world" />*/}
+      {/*  <meta property="og:image" content="/images/logo-dark.svg" />*/}
+      {/*  <meta*/}
+      {/*    property="og:description"*/}
+      {/*    content="Your crypto gateway to the future"*/}
+      {/*  />*/}
+      {/*</head>*/}
       <body
         className={`${inter.variable} ${interDisplay.variable} bg-theme-n-8 font-sans text-[0.9375rem] leading-[1.5rem] text-theme-primary antialiased`}
       >
